7cbc61210f0fa0a221f2201de34816204da3ebcf
Tarmo <n4378@student.jamk.fi>
2021-01-19 21:59:59 +0200

    Added main files

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function About(props) {
+  return (
+    <div>
+      <h1>Tämä on about kohta komponentti</h1>
+    </div>
+  )
+}
+ 
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React from 'react';
 import logo from './logo.svg';
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js'; 
+import Contact from './Contact.js'; 
+import About from './About.js'; 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		  <BrowserRouter>
+      <div className="App">
+  
+        // Set up the Router
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+  
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo Image" />
+          <div className="navigation-sub">
+                                          
+            // Set up the Links
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+  
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+ );
 }
 
 export default App;
